b6be466cb19a24da722a984daeb848220bdcc673
Katrine <chiorioch@gmail.com>
2024-03-15 18:49:01 +0300

    2.1.2: Spring. The first steps. Hello World.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("bean и bean2: " + (bean == bean2));
+        System.out.println("cat и cat2: " + (cat == cat2));
     }
 }
