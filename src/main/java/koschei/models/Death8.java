21eff8fbe23e36f3aa716058b85ef155d9fcf22c
Katrine <chiorioch@gmail.com>
2024-03-25 15:31:40 +0300

    2.1.4: Виды связывания бинов.

---
+++ src/main/java/koschei/models/Death8.java
@@ -0,0 +1,11 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Death8 {
+    @Override
+    public String toString() {
+        return "Вот и умер дед Кощей х.х";
+    }
+}
